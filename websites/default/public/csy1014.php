6c519e92419a289e9ae1da19b810e98bc4bff891
toheeb-orelope <toheeborelope@gmail.com>
2024-09-26 14:18:21 +0100

    First Commit

---
+++ websites/default/public/csy1014.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University of Northampton - Computer Systems</title>
+</head>
+<body>
+		<header>
+			<img src="logo.jpg" alt="logo" />
+			<ul>
+				<li>Year 1
+					<ul>
+						<li><a href="csy1014.html">CSY1014 Computer Systems</a></li>
+						<li><a href="csy1017.html">CSY1017 Computer Communications</a></li>
+						<li><a href="csy1018.html">CSY1018 Internet Technology</a></li>
+						<li><a href="csy1019.html">CSY1019 Software Engineering 1</a></li>
+					</ul>
+				</li>
+				<li>Year 2
+					<ul>
+						<li><a href="csy2001.html">CSY2001 Computer Networks</a></li>
+						<li><a href="csy2002.html">CSY2002 Operating Systems</a></li>
+						<li><a href="csy2006.html">CSY2006 Software Engineering 2</a></li>
+						<li><a href="csy2027.html">CSY2027 Group Project</a></li>
+					</ul>
+				</li>
+				<li>Year 3
+
+					<ul>
+						<li><a href="csy3010.html">CSY3010 Media Technology</a></li>
+						<li><a href="csy3013.html">CSY3013 Software Engineering 3</a></li>
+						<li><a href="csy3015.html">CSY3015 Real Time and and Embedded Systems</a></li>
+						<li><a href="csy4010.html">CSY4010 Computing dissertation</a></li>
+					</ul>
+				</li>
+			</ul>
+		</header>
+		<section></section>
+		<main>
+			<h1>CSY1014 Computer Systems</h1>
+
+<p>This module provides basic knowledge of the hardware and software components that make up a computer system. Modern computer systems not only consist of just the computer, but also include a range of peripherals. The module gives an overview of all the important concepts in preparation for a deeper study of computer systems. </p>
+
+<p>
+Learning environments are offered to enable students to gain a basic comprehension, understanding and appreciation of computer system concepts and technologies, and the skills to use them to the best effect.</p>
+
+
+
+		</main>
+
+		<aside>
+
+			<ul>
+				<li>Year 1
+					<ul>
+						<li><a href="csy1014.html">CSY1014 Computer Systems</a></li>
+						<li><a href="csy1017.html">CSY1017 Computer Communications</a></li>
+						<li><a href="csy1018.html">CSY1018 Internet Technology</a></li>
+						<li><a href="csy1019.html">CSY1019 Software Engineering 1</a></li>
+					</ul>
+				</li>
+				</ul>
+
+		</aside>
+
+		<footer>
+			&copy; Small text at the bottom just because it seems necessary
+		</footer>
+
+	</body>
+</html>
---
+++ websites/default/public/index.php
@@ -0,0 +1,97 @@
+<?php
+$name1 = "Toheeb";
+echo"What is the name I want you to display? {$name1}, $name1";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University of Northampton - Computing</title>
+</head>
+<body>
+		<header>
+			<img src="logo.jpg" alt="logo" />
+			<ul>
+				<li>Year 1
+					<ul>
+						<li><a href="csy1014.html">CSY1014 Computer Systems</a></li>
+						<li><a href="csy1017.html">CSY1017 Computer Communications</a></li>
+						<li><a href="csy1018.html">CSY1018 Internet Technology</a></li>
+						<li><a href="csy1019.html">CSY1019 Software Engineering 1</a></li>
+					</ul>
+				</li>
+				<li>Year 2
+					<ul>
+						<li><a href="csy2001.html">CSY2001 Computer Networks</a></li>
+						<li><a href="csy2002.html">CSY2002 Operating Systems</a></li>
+						<li><a href="csy2006.html">CSY2006 Software Engineering 2</a></li>
+						<li><a href="csy2027.html">CSY2027 Group Project</a></li>
+					</ul>
+				</li>
+
+				</li>
+				<li>Year 3
+
+					<ul>
+						<li><a href="csy3010.html">CSY3010 Media Technology</a></li>
+						<li><a href="csy3013.html">CSY3013 Software Engineering 3</a></li>
+						<li><a href="csy3015.html">CSY3015 Real Time and and Embedded Systems</a></li>
+						<li><a href="csy4010.html">CSY4010 Computing dissertation</a></li>
+					</ul>
+				</li>
+			</ul>
+		</header>
+		<section></section>
+		<main>
+			<h1>Computing at the University of Northampton</h1>
+
+
+			<p>We offer a full range of flexible, part-time and full-time, undergraduate and postgraduate programmes to help you develop key computing skills.</p>
+
+<p>Our courses offer an excellent opportunity to gain employment or to develop skills essential for career enhancement and progression to ensure that you can excel in whichever career you choose to enter.</p>
+
+<p>We welcome all applicants to our courses, including international applications and applications from students with a range of non-traditional educational or professional qualifications.</p>
+
+		</main>
+
+		<aside>
+
+			<ul>
+				<li>Year 1
+					<ul>
+						<li><a href="csy1014.html">CSY1014 Computer Systems</a></li>
+						<li><a href="csy1017.html">CSY1017 Computer Communications</a></li>
+						<li><a href="csy1018.html">CSY1018 Internet Technology</a></li>
+						<li><a href="csy1019.html">CSY1019 Software Engineering 1</a></li>
+					</ul>
+				</li>
+				<li>Year 2
+					<ul>
+						<li><a href="csy2001.html">CSY2001 Computer Networks</a></li>
+						<li><a href="csy2002.html">CSY2002 Operating Systems</a></li>
+						<li><a href="csy2006.html">CSY2006 Software Engineering 2</a></li>
+						<li><a href="csy2027.html">CSY2027 Group Project</a></li>
+					</ul>
+				</li>
+
+				</li>
+				<li>Year 3
+
+					<ul>
+						<li><a href="csy3010.html">CSY3010 Media Technology</a></li>
+						<li><a href="csy3013.html">CSY3013 Software Engineering 3</a></li>
+						<li><a href="csy3015.html">CSY3015 Real Time and and Embedded Systems</a></li>
+						<li><a href="csy4010.html">CSY4010 Computing dissertation</a></li>
+					</ul>
+				</li>
+			</ul>
+
+		</aside>
+
+		<footer>
+			&copy; Small text at the bottom just because it seems necessary
+		</footer>
+
+	</body>
+</html>
